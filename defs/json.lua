648a6fd3555d55fff8e20c124d3548e292677e87
Paulo Carabalone <psoreto@outlook.com>
2025-12-10 20:05:28 -0300

    defs added

---
+++ defs/json.lua
@@ -0,0 +1,14 @@
+--- @meta
+
+--- @class json
+local json = {}
+
+--- @param value any
+--- @return string, string?
+function json.stringify(value) end
+
+--- @param text string
+--- @return any, string?
+function json.parse(text) end
+
+return json
